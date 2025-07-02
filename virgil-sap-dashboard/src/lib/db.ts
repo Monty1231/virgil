7cd60b20f73b9658b8f1dd65d13f1f03a901afb5
Monty1231 <montygoldberg7@gmail.com>
2025-07-02 12:20:56 -0400

    test endpoint + jdbc working

--- virgil-sap-dashboard/src/lib/db.ts
+++ virgil-sap-dashboard/src/lib/db.ts
@@ -1,11 +1,26 @@
 import { Pool } from 'pg'
 
+// const pool = new Pool({
+//   user: 'vadmin', // replace with your actual username
+//   host: process.env.DB_HOST,
+//   database: 'virgilrds', // your actual DB name (you might want to lowercase and remove spaces)
+//   password: process.env.DB_PASSWORD,
+//   port: 5432,
+//   ssl: {
+//     rejectUnauthorized: false, // only for development
+//   },
+// })
+
+// export default pool
+
 const pool = new Pool({
-  user: 'admin',
+  user: process.env.DB_USER,
   host: process.env.DB_HOST,
-  database: 'postgres',
+  database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 })
-
 export default pool
